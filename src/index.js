ba02d0ce52f61203444f1a6d8cefc4c170ee8c74
vanminhnguyenbmt <vanminhnguyenbmt@gmail.com>
2018-10-11 14:02:50 +0700

    add Crypto-js

--- src/index.js
+++ src/index.js
@@ -7,3 +7,24 @@ exports.createSimpleWallet = function (password) {
     var pass = new nem_library_1.Password(password);
     return nem_library_1.SimpleWallet.create(exports.MOSAIC_NAME + "-wallet", pass);
 };
+// const blockHttp = new BlockHttp();
+// blockHttp.getBlockByHeight(1675667).subscribe(block => {
+//     console.log(block);
+// });
+// const chainHttp = new ChainHttp();
+// chainHttp.getBlockchainLastBlock().subscribe(block => {
+//     console.log(block);
+// })
+var accoutHttp = new nem_library_1.AccountHttp();
+accoutHttp.getFromAddress(new nem_library_1.Address('TANXDSNMIMXHOD3IK7I556KPTDXFYBRNEQTTOY7K')).subscribe(function (account) {
+    console.log(account);
+});
+accoutHttp.getFromAddress(new nem_library_1.Address('TDG2CJZTM7X2P6TX4L7M7XO7MJ27QTJ2X4JHZ2WU')).subscribe(function (account) {
+    console.log(account);
+});
+// const userInfo = {
+//     fullName: 'Van Minh Nguyen',
+//     dob: '29/11/1996',
+//     gender: 'male'
+// }
+// console.log(SHA256(JSON.stringify(userInfo)).toString().toUpperCase());
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { SimpleWallet, Password, NEMLibrary, NetworkTypes, AccountHttp, Address } from 'nem-library';
+import { SimpleWallet, Password, NEMLibrary, NetworkTypes, AccountHttp, Address, ChainHttp, NodeHttp, BlockHttp } from 'nem-library';
+import { SHA256 } from 'crypto-js';
 
 NEMLibrary.bootstrap(NetworkTypes.TEST_NET);
 
@@ -8,3 +9,26 @@ export const createSimpleWallet = (password: string): SimpleWallet => {
     const pass = new Password(password);
     return SimpleWallet.create(`${MOSAIC_NAME}-wallet`, pass);
 };
+
+// const blockHttp = new BlockHttp();
+// blockHttp.getBlockByHeight(1675667).subscribe(block => {
+//     console.log(block);
+// });
+
+// const chainHttp = new ChainHttp();
+// chainHttp.getBlockchainLastBlock().subscribe(block => {
+//     console.log(block);
+// })
+
+// const accoutHttp = new AccountHttp();
+// accoutHttp.getFromAddress(new Address('TANXDSNMIMXHOD3IK7I556KPTDXFYBRNEQTTOY7K')).subscribe(account => {
+//     console.log(account);
+// })
+
+// const userInfo = {
+//     fullName: 'Van Minh Nguyen',
+//     dob: '29/11/1996',
+//     gender: 'male'
+// }
+
+// console.log(SHA256(JSON.stringify(userInfo)).toString().toUpperCase());
